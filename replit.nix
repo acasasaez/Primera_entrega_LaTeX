e079f359546626e34abbb22af5c2aa9de5edd434
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2022-09-19 21:38:00 +0000

    zz

--- replit.nix
+++ replit.nix
@@ -1,18 +1,7 @@
 { pkgs }: {
-  deps = [
-    pkgs.python38Full
-  ];
-  env = {
-    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      # Needed for pandas / numpy
-      pkgs.stdenv.cc.cc.lib
-      pkgs.zlib
-      # Needed for pygame
-      pkgs.glib
-      # Needed for matplotlib
-      pkgs.xorg.libX11
+    deps = [
+        pkgs.texlive.combined.scheme-full
+			  pkgs.httplz
+				pkgs.texlab
     ];
-    PYTHONBIN = "${pkgs.python38Full}/bin/python3.8";
-    LANG = "en_US.UTF-8";
-  };
 }
