f6dcf1698c0521f2bafb444815d6262647f74ba8
acasasaez <acasasae@myuax.com>
2022-10-05 22:51:50 +0200

    Update mmh.py

--- ejercicio.py/mmh.py
+++ ejercicio.py/mmh.py
@@ -84,3 +84,45 @@ class HMM():
             states[t] = draw_from(self.A[states[t-1],:])
             observations[t] = draw_from(self.B[states[t],:])
         return observations,states
+
+    
+    def baum_welch_train(self, observations, criterion=0.05):
+        n_states = self.A.shape[0]
+        n_samples = len(observations)
+
+        done = False
+        while not done:
+            
+            alpha = self._forward(observations)
+
+            beta = self._backward(observations)
+
+            xi = np.zeros((n_states,n_states,n_samples-1))
+            for t in range(n_samples-1):
+                denom = np.dot(np.dot(alpha[:,t].T, self.A) * self.B[:,observations[t+1]].T, beta[:,t+1])
+                for i in range(n_states):
+                    numer = alpha[i,t] * self.A[i,:] * self.B[:,observations[t+1]].T * beta[:,t+1].T
+                    xi[i,:,t] = numer / denom
+
+            
+            gamma = np.squeeze(np.sum(xi,axis=1))
+            
+            prod =  (alpha[:,n_samples-1] * beta[:,n_samples-1]).reshape((-1,1))
+            gamma = np.hstack((gamma,  prod / np.sum(prod))) 
+
+            newpi = gamma[:,0]
+            newA = np.sum(xi,2) / np.sum(gamma[:,:-1],axis=1).reshape((-1,1))
+            newB = np.copy(self.B)
+
+            num_levels = self.B.shape[1]
+            sumgamma = np.sum(gamma,axis=1)
+            for lev in range(num_levels):
+                mask = observations == lev
+                newB[:,lev] = np.sum(gamma[:,mask],axis=1) / sumgamma
+
+            if np.max(abs(self.pi - newpi)) < criterion and \
+                            np.max(abs(self.A - newA)) < criterion and \
+                            np.max(abs(self.B - newB)) < criterion:
+                done = 1
+
+            self.A[:],self.B[:],self.pi[:] = newA,newB,newpi
