9a570b212f1676302aa933ea401a767775c12275
acasasaez <acasasae@myuax.com>
2022-10-05 22:36:42 +0200

    Update mmh.py

--- ejercicio.py/mmh.py
+++ ejercicio.py/mmh.py
@@ -5,3 +5,29 @@ class HMM():
         self.A = A
         self.B = B
         self.pi = pi
+
+    def _forward(self, obs_seq):
+        N = self.A.shape[0]
+        T = len(obs_seq)
+
+        F = np.zeros((N,T))
+        F[:,0] = self.pi * self.B[:, obs_seq[0]]
+
+        for t in range(1, T):
+            for n in range(N):
+                F[n,t] = np.dot(F[:,t-1], (self.A[:,n])) * self.B[n, obs_seq[t]]
+
+        return F
+
+    def _backward(self, obs_seq):
+        N = self.A.shape[0]
+        T = len(obs_seq)
+
+        X = np.zeros((N,T))
+        X[:,-1:] = 1
+
+        for t in reversed(range(T-1)):
+            for n in range(N):
+                X[n,t] = np.sum(X[:,t+1] * self.A[n,:] * self.B[:, obs_seq[t+1]])
+
+        return X
