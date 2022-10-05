d4a7b02998e610b43eaa2d946baa96a3e6f903b6
acasasaez <acasasae@myuax.com>
2022-10-05 22:45:08 +0200

    Update mmh.py

--- ejercicio.py/mmh.py
+++ ejercicio.py/mmh.py
@@ -40,8 +40,47 @@ class HMM():
     def state_path(self, obs_seq):
         V, prev = self.viterbi(obs_seq)
 
-        # Build state path with greatest probability
+       
         last_state = np.argmax(V[:,-1])
         path = list(self.build_viterbi_path(prev, last_state))
 
         return V[last_state,-1], reversed(path)
+
+    def viterbi(self, obs_seq):
+        N = self.A.shape[0]
+        T = len(obs_seq)
+        prev = np.zeros((T - 1, N), dtype=int)
+
+        V = np.zeros((N, T))
+        V[:,0] = self.pi * self.B[:,obs_seq[0]]
+
+        for t in range(1, T):
+            for n in range(N):
+                seq_probs = V[:,t-1] * self.A[:,n] * self.B[n, obs_seq[t]]
+                prev[t-1,n] = np.argmax(seq_probs)
+                V[n,t] = np.max(seq_probs)
+
+        return V, prev
+
+    def build_viterbi_path(self, prev, last_state):
+        
+        T = len(prev)
+        yield(last_state)
+        for i in range(T-1, -1, -1):
+            yield(prev[i, last_state])
+            last_state = prev[i, last_state]
+
+
+    def simulate(self, T):
+
+        def draw_from(probs):
+            return np.where(np.random.multinomial(1,probs) == 1)[0][0]
+
+        observations = np.zeros(T, dtype=int)
+        states = np.zeros(T, dtype=int)
+        states[0] = draw_from(self.pi)
+        observations[0] = draw_from(self.B[states[0],:])
+        for t in range(1, T):
+            states[t] = draw_from(self.A[states[t-1],:])
+            observations[t] = draw_from(self.B[states[t],:])
+        return observations,states
